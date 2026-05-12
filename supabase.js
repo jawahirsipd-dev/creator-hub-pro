a9f1a4498d9748b2be61db34c23f805683ff7de2
jawahirsipd-dev <jawahir.sipd@gmail.com>
2026-05-12 00:51:57 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,9 @@
 
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabase = createClient(
+"https://dpjmlasbomyleogiimmy.supabase.co",
+"YOUR_PUBLISHABLE_KEY"
+);
+
+export default supabase;

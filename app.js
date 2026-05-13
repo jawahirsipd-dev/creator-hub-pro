d2eba2437161e8e00e3c5df0d662066828720149
jawahirsipd-dev <jawahir.sipd@gmail.com>
2026-05-13 00:49:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-<a href="${tool.url}" target="_blank" class="visit-btn">
-Visit
-</a>
+window.trackClick = async function(id){
+
+const tool = allTools.find(t => t.id === id);
+
+if(!tool) return;
+
+const newClicks = (tool.clicks || 0) + 1;
+
+await supabase
+.from("tools")
+.update({
+clicks:newClicks,
+trending_score:newClicks
+})
+.eq("id",id);
+
+}

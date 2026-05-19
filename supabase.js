f227b4d3c404458984476e0105c23539684c3982
jawahirsipd-dev <jawahir.sipd@gmail.com>
2026-05-19 02:14:42 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL =
+"https://YOURPROJECT.supabase.co";
 
+const SUPABASE_KEY =
+"YOUR_PUBLISHABLE_KEY";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);

4d791b47c64038c9e2a83f6b8558792340594f42
jawahirsipd-dev <jawahir.sipd@gmail.com>
2026-05-19 20:49:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ try{
 
 const response = await fetch(
 
-"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=YOUR_GEMINI_API_KEY",
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?AIzaSyAd4sPxJ2lMtrkCjmJ41QUl0KeFD2JU4ZE",
 
 {
 
